572cc0529408bc760387ab8efab2a3680123c67c
Abanoub Adel <aadil@taqeem.gov.sa>
2025-09-29 18:23:30 +0300

    update reporter in playwright.config

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
    reporter: [
-    ['list'],
+    ['html'],
     ['allure-playwright', { outputFolder: 'allure-results' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
